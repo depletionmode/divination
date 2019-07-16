b36ef2364f28bc04cbac5f03ec134f15f72fc3fa
David Kaplan (DEPLETIONMODE) <dakapl@microsoft.com>
2019-07-17 00:34:28 +0100

    latest spi

--- spi.py
+++ spi.py
@@ -1,5 +1,7 @@
 import struct
 
+### I apologise profusely for the messiness of this code - @depletionmode ###
+
 print("""
 AMD Family 17h - SPI ROM interface test code by @depletionmode
       """)
@@ -11,19 +13,22 @@ print('LPC Bridge @ D14F3')
 # todo
 # pci_cfg = ReadPciConfig(0, 14, 3)
 # spi_base_addr = (pci_cfg[0xa0] >> 6) << 6
+# spi_rom_enable = (pci_cfg[0xa0] >> 1) & 1
 spi_base_addr = 0xfec10000
+spi_rom_enable = 1
 print('SPI_BASE_ADDR @ D14F3xA0[31:6] : {}'.format(hex(spi_base_addr)))
+print('SPI_ROM_ENABLE @ D14F3xA0[1] : {}'.format(spi_rom_enable == 1))
 
 spi_bar = MemoryObject(0xfec10000, 0x100, MemoryType.IoSpace)
 
 spi_x00 = struct.unpack("<I", spi_bar[:3])
 print("""
 SPIx00: {:#x} {:#032b}
-SPI_READMODE @ SPIx00[30:29 + 18] : {:#b}
-SPI_CLKGATE @ SPIx00[28] : {:#b}
+--> SPI_READMODE @ SPIx00[30:29 + 18] : {:#b}
+--> SPI_CLKGATE @ SPIx00[28] : {:#b}
 """.format(
     spi_x00,
     spi_x00,
-    (((spi_x00 >> 29) & 3) << 1) | ((spi >> 18) & 1)
+    (((spi_x00 >> 29) & 3) << 1) | ((spi_x00 >> 18) & 1)
     (spi_x00 >> 28) & 1
 ))
