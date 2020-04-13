6608254dc6d519d262ce96ae1b9e2eded0076262
David Kaplan <dakapl@microsoft.com>
2020-04-13 08:12:59 +0100

    Add MANIFEST.in and bump major version

--- setup.py
+++ setup.py
@@ -5,16 +5,18 @@ with open('README.rst', 'r') as f:
 
 setuptools.setup(
     name='divination',
-    version='0.2.0',
+    version='2.0.0',
     author='David Kaplan <@depletionmode>',
     description='Python module for hardware and physmem inspection',
     long_description=long_description,
+    long_description_content_type='text/x-rst',
     url='https://github.com/depletionmode/divination',
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent"
+        "Operating System :: OS Independent",
         "License :: OSI Approved :: MIT License",
         "Development Status :: 4 - Beta"
-    ]
+    ],
+    include_package_data=True
 )
